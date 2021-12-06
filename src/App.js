f76dbc4b98c5af83290767648d699b6d5bdd265d
disenodc <disenodc@gmail.com>
2021-12-06 13:47:23 -0300

    ecommerce versión 1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {NavBar} from './components/NavBar/NavBar';
+
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className="titulo">
+      <svg className="App-logo" xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-apple" viewBox="0 0 16 16">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
+  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+</svg>
+      </svg> Ecommerce ReactJs</h1>
+        
+         <div >
+          
+        </div>
+         
+        </header>
+      </div>
   );
 }
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,27 @@
+import './NavBar.css'
+
+import CartWidget from '../CartWidget';
+import React from 'react';
+
+export const NavBar = () => {
+    return (
+
+        <div className="NavBar">
+            <ul className="App-menu">
+              <li className="menu-link">
+                Inicio
+              </li>
+              <li className="menu-link">
+                Nosotros
+              </li>
+              <li className="menu-link">
+                Productos
+              </li>
+              <li className="menu-link">
+                Contactos
+              </li>
+            </ul>
+            <CartWidget />
+        </div>
+    );
+}
