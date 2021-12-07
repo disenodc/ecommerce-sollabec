93780afe2f77e2cd19085404a6b91f093aca6da5
disenodc <disenodc@gmail.com>
2021-12-07 14:43:45 -0300

    agregue ItemListContainer

---
+++ src/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ItemListContainer.css'
+
+export default function ItemListContainer({titulo, catalogo}) {
+    return (
+        <div>
+            <h2>{titulo}</h2>
+        </div>
+    )
+}
+
